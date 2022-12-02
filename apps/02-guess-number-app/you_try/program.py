f50a667a77cbe8076656d18d34003d46c34c8d6f
psweeting <patrick.sweeting@digital.justice.gov.uk>
2022-12-02 07:46:55 +0000

    Commit completed training

---
+++ apps/02-guess-number-app/you_try/program.py
@@ -0,0 +1,22 @@
+import random
+
+print('----------------')
+print('Guess that number game')
+print('----------------')
+print()
+
+the_number = random.randint(0, 100)
+guess = -1
+
+name = input('Player what is your name? ')
+
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+
+    if guess < the_number:
+        print('Sorry {}, your guess of {} was too LOW'.format(name, guess))
+    elif guess > the_number:
+        print('Sorry {}, your guess of {} was too HIGH'.format(name, guess))
+    else:
+        print('Nice one {}, the number was {}'.format(name, guess))
