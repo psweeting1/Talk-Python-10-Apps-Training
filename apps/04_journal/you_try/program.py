fcd11450e9a5a4074a4d6829c11351e213149913
psweeting <patrick.sweeting@digital.justice.gov.uk>
2022-12-02 10:32:53 +0000

    Completed journal chapter

--- apps/04_journal/you_try/program.py
+++ apps/04_journal/you_try/program.py
@@ -1,3 +1,6 @@
+import journal
+
+
 def main():
     print_header()
     run_event_loop()
@@ -11,10 +14,12 @@ def print_header():
 
 def run_event_loop():
     print('What do you want to do with your journal?')
-    cmd = None
-    journal_data = []  # list()
+    cmd = 'EMPTY'
+    journal_name = 'default'
+    journal_data = journal.load(journal_name)
 
-    while cmd != 'x':
+    # if enter is hit cmd takes an empty string
+    while cmd != 'x' and cmd:
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
         cmd = cmd.lower().strip()
 
@@ -22,10 +27,11 @@ def run_event_loop():
             list_entries(journal_data)
         elif cmd == 'a':
             add_entry(journal_data)
-        elif cmd != 'x':
+        elif cmd != 'x' and cmd:
             print("Sorry, we don't understand '{}'.".format(cmd))
 
     print('Done, goodbye.')
+    journal.save(journal_name, journal_data)
 
 
 def list_entries(data):
@@ -33,12 +39,17 @@ def list_entries(data):
     entries = reversed(data)
 
     for index, entry in enumerate(entries):
-        print('* [{}] {1}'.format(index+1, entry))
+        print('* [{}] {}'.format(index + 1, entry))
 
 
 def add_entry(data):
     text = input('Type your entry, <enter> to exit: ')
-    data.append(text)
+    # data.append(text)
+    journal.add_entry(text, data)
 
 
-main()
+print("__file__ " + __file__)
+print("__name__ " + __name__)
+
+if __name__ == '__main__':
+    main()
